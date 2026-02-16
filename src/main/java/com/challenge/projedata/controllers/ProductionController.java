ef5c8fce4fea3f6ef7b25e8ca57ca28a8954df9a
joao <joaovictorfarias1610@gmail.com>
2026-02-15 22:08:53 -0300

    cors

--- src/main/java/com/challenge/projedata/controllers/ProductionController.java
+++ src/main/java/com/challenge/projedata/controllers/ProductionController.java
@@ -2,12 +2,14 @@ package com.challenge.projedata.controllers;
 
 import com.challenge.projedata.dtos.ProductionResponseDTO;
 import com.challenge.projedata.services.ProductionService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/production")
 public class ProductionController {
